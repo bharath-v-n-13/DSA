6c3bbde20a88c82634abef0ed9e0d73e22a5cf66
BHARATH  V N <119437857+bharath-v-n-13@users.noreply.github.com>
2025-07-09 20:48:18 +0530

    Time: 1 ms (86.28%) | Memory: 41.1 MB (30.70%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,7 +1,25 @@
 class Solution {
     public int mySqrt(int x) {
-        int ans =(int)Math.sqrt(x);
+        if (x == 0 || x == 1) {
+            return x;
+        }
+
+        int start = 1, end = x, ans = 0;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            // Avoiding multiplication overflow
+            if ((long)mid * mid == x) {
+                return mid;
+            } else if ((long)mid * mid < x) {
+                ans = mid;         // store the floor of sqrt
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
         return ans;
-        
     }
 }
