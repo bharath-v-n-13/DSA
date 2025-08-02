76147403ca25339594685adacb3d328c17f64171
BHARATH  V N <119437857+bharath-v-n-13@users.noreply.github.com>
2025-08-02 21:27:06 +0530

    Time: 538 ms (61.63%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,4 +1,1 @@
-select c.name as Customers
-from customers c LEFT JOIN  Orders O
-ON c.id=o.customerid
-WHERE o.customerid is null;
+select c.name as Customers from customers c LEFT JOIN  Orders O ON c.id=o.customerid WHERE o.customerid is null;
