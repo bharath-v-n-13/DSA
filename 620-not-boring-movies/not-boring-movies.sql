16a177274d5d85a62174e238383328b75dc893e3
BHARATH  V N <119437857+bharath-v-n-13@users.noreply.github.com>
2025-09-17 12:24:17 +0530

    Time: 206 ms (98.87%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,3 +1,1 @@
-select id,movie,description,rating
-from cinema 
-where id%2<>0 and description <> "boring" order by  rating desc;
+select * from cinema where id%2=1 and description<>"boring" order by rating desc;
