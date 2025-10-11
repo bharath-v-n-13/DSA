aeef9dd368c92158f18145ce0ef6776a4385a066
BHARATH  V N <119437857+bharath-v-n-13@users.noreply.github.com>
2025-10-11 08:07:41 +0530

    Time: 144 ms (5.03%) | Memory: 45.5 MB (99.58%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,33 +1,14 @@
 class Solution {
-    public static void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
     public void moveZeroes(int[] nums) {
-        int j = -1;
-        
-        // find first zero
+        int j = 0; 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                j = i;
-                break;
-            }
-        }
-
-        // if there are no zeros
-        if (j == -1) return;
-
-        // move non-zero elements ahead
-        for (int i = j + 1; i < nums.length; i++) {
             if (nums[i] != 0) {
-                swap(nums, i, j);
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
                 j++;
             }
         }
-
-        // print result
         for (int num : nums) {
             System.out.print(num + " ");
         }
