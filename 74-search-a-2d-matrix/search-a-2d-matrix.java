67f3a3cb9542a84a25da287c8a48114648553960
BHARATH  V N <119437857+bharath-v-n-13@users.noreply.github.com>
2025-10-15 17:24:32 +0530

    Time: 1 ms (3.06%) | Memory: 42.1 MB (82.52%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        boolean flag = false;
+       for (int i = 0; i < matrix.length; i++) {
+    Arrays.sort(matrix[i]);
+}
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(matrix[i][j]==target){
+                    flag = true;
+                }
+            }
+        }
+        return flag;
+        
+    }
+}
