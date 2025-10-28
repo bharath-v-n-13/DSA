0ea1d23f3c3e0f87f7ec2ab1acf7d72a038567e2
BHARATH  V N <119437857+bharath-v-n-13@users.noreply.github.com>
2025-10-28 11:32:11 +0530

    Time: 1 ms (58.17%) | Memory: 41.2 MB (95.90%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,20 +1,21 @@
+import java.util.*;
+
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
         Arrays.sort(strs);
+
         String str1 = strs[0];
-        String str2 = strs[strs.length-1];
+        String str2 = strs[strs.length - 1];
+
         int index = 0;
-        while(index<str1.length())
-        {
-            if(str1.charAt(index)==str2.charAt(index))
-            {
-                index++;
-            }
-            else
-            {
-                break;
-            }
+        int minLen = Math.min(str1.length(), str2.length());
+
+        while (index < minLen && str1.charAt(index) == str2.charAt(index)) {
+            index++;
         }
-        return index==0?"":str1.substring(0,index);
+
+        return str1.substring(0, index);
     }
 }
